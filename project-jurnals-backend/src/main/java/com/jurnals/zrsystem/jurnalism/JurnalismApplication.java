4f19e285eec4e0ea27bd2004d4e5531b1659b25a
rakib <zellurakib1234@gmail.com>
2020-06-20 01:25:47 +0600

    Init

---
+++ project-jurnals-backend/src/main/java/com/jurnals/zrsystem/jurnalism/JurnalismApplication.java
@@ -0,0 +1,13 @@
+package com.jurnals.zrsystem.jurnalism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurnalismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JurnalismApplication.class, args);
+    }
+
+}
---
+++ project-jurnals-backend/src/test/java/com/jurnals/zrsystem/jurnalism/JurnalismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jurnals.zrsystem.jurnalism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurnalismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
